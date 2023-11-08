888a91248d262472a0fb035f2d7d8e7027e07919
Ketanb08 <ketanbajaj72@gmail.com>
2023-11-08 21:54:02 +0530

    update2

--- app/models.py
+++ app/models.py
@@ -1,10 +1,26 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+def val_climate(value):
+    if(value=="hot" or value=="cold" or value=="rainy" or value=="humid"):
+        return 1
+    else:return 0
+def val_range(value):
+    if( value<100 or value>1000):
+        return -1
+    else:return 0
+def is_valid(climate,area):
+    a=val_climate(climate)
+    b=val_range(area)
+    if(a==0 or b==0):
+        return -1
+    return 1
 class Climate(models.Model):
-    climate=models.CharField(max_length=5)
-    temperature=models.CharField(max_length=3)
-    area=models.CharField(max_length=5)
-    humidity=models.CharField(max_length=2)
-    chances=models.CharField(max_length=2)
+    climate=models.CharField(max_length=7,validators=[val_climate])
+    temperature=models.IntegerField()
+    area=models.IntegerField(validators=[val_range])
+    humidity=models.IntegerField()
+    chances=models.IntegerField()
     date=models.DateField()
+   
--- app/views.py
+++ app/views.py
@@ -3,31 +3,42 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login,logout
 from datetime import datetime
-from app.models import Climate
+from app.models import Climate,is_valid
+from django.core.exceptions import ValidationError
 # Create your views here.
 
 def home(request):
+    status=1
+    all1=0
     if request.method == "POST":
         area = request.POST.get("area", "Enter valid Area code")
         climate=request.POST.get("climate","Enter valid Climate Type")
         if(area=="" and climate==""):
             info2=Climate.objects.all()
             print(info2.count())
-            return render(request, "index.html", {"info2": info2})
+            all1=1
+            return render(request, "index.html", {"info2": info2,"status":status,"all1":all1})
         elif (area=="Enter valid Area code") or (area==""):
             info=Climate.objects.filter(climate=climate).first()
         elif(climate=="Enter valid Climate Type" or (climate=="")):
             info=Climate.objects.filter(area=area).first()
         else:
             info=Climate.objects.filter(climate=climate,area=area).first()
-        return render(request, "index.html", {"info": info})
-    return render(request,'index.html')
+            print(info)
+            if info==None:
+                status=0
+        print("status :",status)
+        print ("Info :",info)
+        if info==None:
+            status=0
+        return render(request, "index.html", {"info": info,"status":status})
+    return render(request,'index.html',{"status":status})
 
 def services(request):
     return render(request,'services.html')
 
 def about(request):
-    return render(request,'about.html')
+    return render(request,'temp.html')
 
 def loginUser(request):
     return render(request,'login.html')
@@ -50,25 +61,49 @@ def data(request):
         return redirect('login')
 
 def setdata(request):
+    exception=None
+    success=None
     if request.method=="POST":
         climate=request.POST.get('climate')
         temperature=request.POST.get('temperature')
         area=request.POST.get('area')
+        print(climate)
+        print(type(climate))
         humidity=request.POST.get('humidity')
         chances=request.POST.get('chances')
         condition=Climate(climate=climate,temperature=temperature,area=area,chances=chances,humidity=humidity,date=datetime.today())
-        condition.save()
-    return render(request,'data.html')
+        print("output",is_valid(climate,int(area)))
+        if is_valid(climate,int(area))==1:
+            condition.save()
+            success="success"
+        else:
+            exception="Invalid Form Fields"
+            return render(request,'data.html',{"exception":exception})
+    return render(request,'data.html',{"success":success})
     
 def changedata(request):
+    old1=None
+    new1=None
+    area1=None
+    success1=None
     if request.method=="POST":
         area=request.POST.get("area","No Information")
         obj=Climate.objects.filter(area=area).first()
-        newclimate=request.POST.get("newclimate","No Information")
-        oldclimate=request.POST.get("oldclimate","No Information")
+        newclimate=request.POST.get("newclimate")
+        oldclimate=request.POST.get("oldclimate")
+        if(oldclimate==""):
+            old1=1
+            return render(request,'data.html',{"old1":old1})
+        if(newclimate==""):
+            new1=1
+            return render(request,'data.html',{"new1":new1})
+        if(area==""):
+            area1=1
+            return render(request,'data.html',{"area1":area1})
         obj.climate=newclimate
         obj.save()
-    return render(request,'data.html')
+        success1=1
+    return render(request,'data.html',{"success1":success1})
 
 def contact(request):
     return render(request,'contact.html')
